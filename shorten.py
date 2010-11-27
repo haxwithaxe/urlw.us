d7c698489ef51505bd6a7741c6fdf311b4cdb653
Chris Koepke <ckoepke@innovetech.com>
2010-11-26 19:49:56 -0500

    updating code and adding the important part of the apache config

--- shorten.py
+++ shorten.py
@@ -3,66 +3,80 @@
 url shortener script
 '''
 
-import os,sys,cgi,cgitb,random,string
+import os,sys,cgi,cgitb,random,string,re
 cgitb.enable()
 
+gVars = {}
+httpsre = re.compile('^https://')
+httpre = re.compile('^http://')
 SHORTURL = None
-NAMECOUNT = 10
+APIURL = None
+gVars['NAMECOUNT'] = 10
 LEN = 5
 POP = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z','0','1','2','3','4','5','6','7','8','9']
-SCRIPT_NAME = os.environ['SCRIPT_NAME']
-DIR = '/var/www/'
-FORM_STR = '''<div style="text-align:center">
-<b>Enter the long url and the short name you want.</b><br/><br/>
-<form action='''+SCRIPT_NAME+''' method="POST" enctype="multipart/form-data">
-Long URL: <input name="longurl" type="text"/><br/>
-Short Name: <input name="shorturl" type="text"/><br/>
-<input type="submit" value="submit"/>
-</form>
-</div>'''
+DIR = '/var/www/urlw.us/'
+TMPL = 'main.tmpl'
 ERROR = [False,'wut error?']
 
+def uri_type(url):
+   if httpsre.search(url):
+      return url
+   elif httpre.search(url):
+      return url
+   else:
+      return 'http://'+url
+
 def clean_url(url):
-   cleanurl = url.replace('/','').replace('{','').replace('(','').replace('[','').replace('|','')
+   cleanurl = url.replace('"','').replace('/','').replace('{','').replace('(','').replace('[','').replace('|','')
    return cleanurl
 
-def get_short_name():
+def get_short_name(gVars):
    short = ''.join(str(x) for x in random.sample(POP,LEN))
-      if NAMECOUNT == 0:
-         return short
-      elif os.path.exists(DIR+short):
-         NAMECOUNT += 1
-         short = get_short_name()
+   if gVars['NAMECOUNT'] == 0:
+      return short
+   elif os.path.exists(DIR+short):
+      gVars['NAMECOUNT'] += 1
+      short = get_short_name(gVars)
+   return short
+
+output = open(DIR+TMPL,'r').read()
+
+output = output.replace('<!--ACTION-->',os.environ['SCRIPT_NAME'])
 
 form = cgi.FieldStorage()
 if form.has_key('shorturl'):
    if not form['shorturl'].value:
-         shorturl = get_short_name()
+         shorturl = get_short_name(gVars)
    else:
       shorturl = clean_url(form['shorturl'].value)
       if os.path.exists(DIR+shorturl):
-         shorturl = get_short_name()
+         shorturl = get_short_name(gVars)
          ERROR = [True,'Name Taken']
 else:
-   shorturl = get_short_name()
+   shorturl = get_short_name(gVars)
 
 if form.has_key('longurl'):
-   longurl = form['longurl'].value
+   longurl = uri_type(form['longurl'].value)
    redirect = '<html><head><meta http-equiv="refresh" content="0;url='+longurl+'" /></head><body /></html>'
-   open('/var/www/'+shorturl+'.html','w').write(redirect)
+   open(DIR+shorturl+'.html','w').write(redirect)
    SHORTURL = os.environ['SERVER_NAME']+'/'+shorturl
+
+if form.has_key('url'):
+   longurl = uri_type(form['url'].value)
+   redirect = '<html><head><meta http-equiv="refresh" content="0;url='+longurl+'" /></head><body /></html>'
+   open(DIR+shorturl+'.html','w').write(redirect)
+   APIURL = os.environ['SERVER_NAME']+'/'+shorturl
+   
+
+if SHORTURL and not ERROR[0]:
+   output = output.replace('<!--RESULT-->','<div id="shorturl"><a href="http://'+SHORTURL+'">http://'+SHORTURL+'</a></div>')
+
+if APIURL and not ERROR[0]:
+   output = APIURL
+
+if ERROR[0]:
+   output = output.replace('<!--ERROR-->','ERROR: '+ERROR[1])
 
 print('Content-Type: text/html\n\n\n')
 
-print('<table><tr><td>')
-print(FORM_STR)
-
-if SHORTURL and not ERROR[0]:
-   print('<p>Here is you shortened url:')
-   print(SHORTURL)
-   print('</p>')
-
-if ERROR[0]:
-   print('<div>ERROR: '+ERROR[1]+'</div>')
-
-print('</td></tr></table>')
+print(output)
